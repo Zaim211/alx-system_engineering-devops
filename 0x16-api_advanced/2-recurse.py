ae7a8e8daa30238bebb9ee922cfa9e2be4165d52
Zaim211 <yousseftitazaim@gmail.com>
2024-01-16 12:21:31 -0800

    update

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -5,7 +5,7 @@ list containing the titles of all hot articles for a given subreddit.
 from requests import get
 
 
-def recurse(subreddit, hot_list=[], after="", count=0):
+def recurse(subreddit, hot_list=[], after=None):
     """
     recursive function that queries the Reddit API and returns 
     a list containing the titles of all hot articles for a given subreddit.
