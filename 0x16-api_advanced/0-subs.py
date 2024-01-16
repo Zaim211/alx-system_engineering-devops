26a9b380895f4b46ee93897341bd2787b7eeb70a
Zaim211 <yousseftitazaim@gmail.com>
2024-01-16 09:11:55 -0800

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,10 @@ def number_of_subscribers(subreddit):
     function that queries the Reddit API
     and returns the number of subscribers
     """
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
-
     headers = {'User-agent': 'Google Chrome Version 120.0.6099.217'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    try:
-        return respnse.json().get('data').get('subscribers')
-    except Exception:
+    if response.status_code != 200:
         return 0
+    subscribers = response.json().get('data').get('subscribers')
+    return subscribers
