31fbe4a016608283ea1da024345513a52729ba98
Zaim211 <yousseftitazaim@gmail.com>
2024-01-17 19:18:36 +0000

    Strace is your friend

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Using strace, find out why Apache is returning a 500 error.
+# And fix it and then automate it using Puppet
+
+exec { 'fix-wordpress':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  path    => ['/bin','/usr/bin']
+} 
